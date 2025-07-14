3f2153fa98e5ecc2f8ab61f5c46feb098d55a871
Alexander Leshko <leshkoan@gmail.com>
2025-07-14 15:38:54 +0400

    Добавил иконку приложения

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:multiplication_wizard/screens/score_records_page.dart';
 import 'package:multiplication_wizard/screens/number_picker_page.dart';
 import 'package:multiplication_wizard/game_logic.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const MultiplicationWizardApp());
+  });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MultiplicationWizardApp extends StatelessWidget {
+  const MultiplicationWizardApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -43,13 +50,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   static final gameLogic = GameLogic();
-  // List<List<bool>> _isFlipped = List.generate(
-  //   8,
-  //   (_) => List.generate(8, (_) => true),
-  // );
-  // Timer? _timer;
-  //int _elapsedSeconds = 0;
-  // bool _gameStarted = false;
   final TextEditingController _nameController = TextEditingController();
   final FocusNode _nameFocusNode = FocusNode();
 
@@ -58,33 +58,10 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     gameLogic.initializeFlipped();
     gameLogic.loadScoreRecords();
-    //        setState(() {
-    ///!!!
-    //        });
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _showWelcomeDialog();
     });
   }
-
-  // void _initializeFlipped() {
-  // final random = Random();
-  // List<Point<int>> points = [];
-  // while (points.length < 10) {
-  //   int row = random.nextInt(8);
-  //   int col = random.nextInt(8);
-  //   Point<int> newPoint = Point(row, col);
-  //   if (!points.contains(newPoint)) {
-  //     points.add(newPoint);
-  //   }
-  // }
-
-  ///!!!   setState(() {
-  // _isFlipped = List.generate(8, (_) => List.generate(8, (_) => true));
-  // for (var point in points) {
-  //   _isFlipped[point.x][point.y] = false;
-  // }
-  ///!!!    });
-  ///}
 
   @override
   void dispose() {
